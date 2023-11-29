209907fdb1c074d09bae3dd588bc29d9b149b948
jamZara9 <150896976+jamZara9@users.noreply.github.com>
2023-11-29 09:49:17 +0900

    .

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,3 @@
-/*!
-* Start Bootstrap - Full Width Pics v5.0.6 (https://startbootstrap.com/template/full-width-pics)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-full-width-pics/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+function layer_toggle(obj) { 
+    if (obj.style.display=='none') obj.style.display = 'block'; 
+    else if (obj.style.display=='block') obj.style.display = 'none'; }
