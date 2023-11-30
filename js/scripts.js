9326fcfc6a3c6ea1601b4a229e933b4f995c9091
jamZara9 <150896976+jamZara9@users.noreply.github.com>
2023-11-30 22:24:17 +0900

    .
    .

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,26 @@
-function layer_toggle(obj) { 
-    if (obj.style.display=='none') obj.style.display = 'block'; 
-    else if (obj.style.display=='block') obj.style.display = 'none'; }
+function dropdown_close(btn) { 
+    
+    let dropdown = btn.parentElement.parentElement.getElementsByClassName("dropdown-contents")[0]
+    let scrollHeight = dropdown.scrollHeight
+    console.log("셋업 전"+scrollHeight)
+    dropdown.setAttribute("style", "height:"+ scrollHeight + "px")
+
+    var doClose = function(btn, dropdown) {
+        console.log('닫기')
+        btn.classList.remove("dropdown-active")
+        btn.setAttribute("onClick", "dropdown_open(this)")
+        dropdown.setAttribute("style", "height: 0px")
+    }
+
+    setTimeout(() => doClose(btn, dropdown), 1)
+}
+
+
+function dropdown_open(btn){
+    let dropdownComponents = btn.parentElement.parentElement.getElementsByClassName("dropdown-contents")[0]
+    let scrollHeight = dropdownComponents.scrollHeight
+    btn.classList.add("dropdown-active")
+    btn.setAttribute("onClick", "dropdown_close(this)")
+    dropdownComponents.setAttribute("style", "height:"+ scrollHeight + "px")
+    setTimeout(() => {dropdownComponents.setAttribute("style", "height:auto")}, 1001)
+}
